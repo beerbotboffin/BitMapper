e6968f22a44026404c8032f92b67172cb83c284b
beerbotboffin <github@shanefrostdesign.com>
2014-11-30 15:51:20 -0800

    Source Files

---
+++ BitMapper/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BitMapper
+{
+    partial class frmBitMapper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBitMapper));
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(124, 564);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(86, 35);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(21, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 107);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(216, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(598, 575);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(59, 24);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(59, 50);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(829, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // frmBitMapper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 620);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmBitMapper";
+            this.Text = "BitMapper";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ BitMapper/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BitMapper
+{
+    public partial class frmBitMapper : Form
+    {
+        public frmBitMapper()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            // Open the file dialog and select a file
+            string path;
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                // User has selected a valid file.
+                path = file.FileName;
+
+                // Open the selected file.
+                Image image = Image.FromFile(path);
+                Bitmap myBitmap = new Bitmap(path);
+
+                // Adjust the picturebox to match the selected file.
+                pictureBox1.Image = image;
+                pictureBox1.Height = image.Height;
+                pictureBox1.Width = image.Width;
+                txtHeight.Text = image.Height.ToString();
+                txtWidth.Text = image.Width.ToString();
+
+                richTextBox1.Text = "{" + '\n';
+
+                int x = 0;
+                int xMax = image.Width - 1;
+                int yMax = image.Height - 1;
+                int bitValue = 1;
+                int bitTotal = 0;
+                string Target = "Color [A=255, R=0, G=0, B=0]";
+                string bitV = "";
+                Color pixelColor = myBitmap.GetPixel(0, 0);
+
+                // process each of the rows.
+                for (int y = 0; y <= yMax; y++)
+                {
+                    // Check the row number and add line feed if 
+                    if (IsDivisble(y, 8) == true)
+                    {
+                        richTextBox1.Text = richTextBox1.Text + '\n';
+                    }
+
+                    // Process each pixel on the row.
+                    for (int byteVal = 0; byteVal < (xMax + 1) / 8; byteVal++)
+                    {
+                        for (int count = 0; count <= 7; count++)
+                        {
+                            pixelColor = myBitmap.GetPixel((byteVal * 8) + count, y);
+                            bitV = pixelColor.ToString();
+                            if (Target == bitV)
+                            {
+                                bitTotal = bitTotal + bitValue;
+                            }
+                            bitValue *= 2;
+                        }
+                        richTextBox1.Text = richTextBox1.Text + "0x" + bitTotal.ToString("X") + ',';
+                        bitValue = 1;
+                        bitTotal = 0;
+                    }
+                    richTextBox1.Text = richTextBox1.Text + '\n';
+                    bitTotal = 0;
+                }
+            }
+
+
+            
+        }
+
+        public bool IsDivisble(int x, int n)
+        {
+            return (x % n) == 0;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BitMapperAboutBox b = new BitMapperAboutBox();
+            b.Show();
+        }
+        
+    }
+}
